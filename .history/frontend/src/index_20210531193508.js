b9c23cbe45e0a27fa1fadcbadc40daf071078b72
Eran Grady <erangrady3.0@gmail.com>
2021-06-01 12:57:27 +0300

    Create a Redux Store

--- .history/frontend/src/index_20210531193508.js
+++ .history/frontend/src/index_20210531193508.js
@@ -1,20 +1,16 @@
 // React packages
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 // Styles
 import './bootstrap.min.css'
 import './index.css';
 // High level Components
 import App from './App';
-// Store
-import store from './store.js'
-
 
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
